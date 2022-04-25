e68dc7a1f6f93740710ed1c6434b71e9c3f120bd
Emanuelit <emanuelitavelino@gmail.com>
2022-04-25 18:40:05 -0300

    iniciando projeto react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css'
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://preview.redd.it/lnmh7c4kelr81.png?width=640&crop=smart&auto=webp&s=5ee4685cf5ff5f5adfc64b71d5728dfe6d33e05a" alt="Imagem Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
