927b3762f273e000856a8ed4af6120cfa9afafed
Emanuelit <emanuelitavelino@gmail.com>
2022-04-27 19:21:28 -0300

    atualizando dependencias

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Typography, Box, Grid, Button} from '@material-ui/core';
+import {Typography, Grid, Button, Box} from '@material-ui/core';
 import './Home.css';
 
 function Home() {
