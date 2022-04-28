1e4c498686a41069bc3c47bbd169d37bcba4f273
Emanuelit <emanuelitavelino@gmail.com>
2022-04-28 11:20:19 -0300

    adicionando rotas e novas dependencias

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import './App.css';
+import Login from './paginas/login/Login';
 
 function App() {
   return (
-  <>
-  <Navbar />
-  <Home />
-  <Footer />
-  </>
+    <Router>
+    <Navbar />
+    <Routes>
+
+      <Route path="/" element={<Login  />} />
+
+      <Route path="/home" element={<Home />} />
+
+      <Route path="/login" element={<Login />} />
+
+    </Routes>
+    <Footer />
+  </Router>
   );
 }
 
