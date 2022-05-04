b5a7755e022727bd139389290f23f658b78a5807
Emanuelit <emanuelitavelino@gmail.com>
2022-05-03 23:03:11 -0300

    Comunicação com o BackEnd e criação da TabPostagem e ListaTema, outras modificações

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
@@ -7,23 +7,33 @@ import './App.css';
 import Login from './paginas/login/Login';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import ListaTema from './components/temas/listaTema/ListaTema';
+
+
 function App() {
   return (
     <Router>
-    <Navbar />
-    <Routes>
+      <Navbar />
+      <Routes>
 
-      <Route path="/" element={<Login  />} />
+        <Route path="/" element={<Login  />} />
 
-      <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Home />} />
 
-      <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
 
-      <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+        <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
 
-    </Routes>
-    <Footer />
-  </Router>
+        <Route path="/temas" element={<ListaTema />} />
+
+        <Route path="/posts" element={<ListaPostagem />} />
+
+
+      </Routes>
+      <Footer />
+    </Router>
+
   );
 }
 
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,8 +1,9 @@
-interface User {
-    id: number;
-    nome: string;
-    usuario: string;
-    senha: string;
+interface User{
+    id: number
+    nome: string
+    usuario: string
+    senha: string
+    foto: string
 }
 
-export default User;
+export default User
