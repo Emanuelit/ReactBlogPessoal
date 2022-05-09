2127f149ec5e130c1803f9b438d338869dc891de
Emanuelit <emanuelitavelino@gmail.com>
2022-05-09 14:31:30 -0300

    Atualizando Redux e paginas

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,12 @@ import CadastroPost from './components/postagens/cadastroPostagem/CadastroPost';
 import CadastroTema from './components/temas/cadastroTema/CadastroTema';
 import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 function App() {
   return (
+    <Provider store={store}>
     <Router>
       <Navbar />
       <div style={{ minHeight: '100vh' }}>
@@ -54,6 +57,7 @@ function App() {
         </div>
       <Footer />
     </Router>
+    </Provider>
 
   );
 }
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,15 +1,18 @@
 import React, {useState, useEffect, ChangeEvent} from 'react';
 import { Box, Grid, Typography, TextField, Button } from '@material-ui/core';
 import {Link, useNavigate} from 'react-router-dom';
-import useLocalStorage from 'react-use-localstorage';
+
 import { login } from '../../services/Service';
 import UserLogin from '../../models/UserLogin';
 import './Login.css';
+import { useDispatch } from 'react-redux';
+import { addToken } from "../../store/tokens/actions";
 
 
 function Login(){
     let history = useNavigate();
-    const [token, setToken] = useLocalStorage('token');
+    const dispatch = useDispatch();
+    const [token, setToken] = useState('');
     const [userLogin, setUserLogin] = useState<UserLogin>(
         {
             id: 0,
@@ -29,6 +32,7 @@ function Login(){
 
     useEffect(() =>{
         if(token !== ""){
+            dispatch(addToken(token));
             history('/home')
         }
     },[token])
